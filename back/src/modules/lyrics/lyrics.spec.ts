ecba2ad11b7499ee53766ae6bfb702314eeb4132
nmellinger <nicolas.mellinger@terascop.com>
2025-01-22 17:10:54 +0100

    generateLyrics will take api key as arg + test

--- back/src/modules/lyrics/lyrics.spec.ts
+++ back/src/modules/lyrics/lyrics.spec.ts
@@ -3,6 +3,22 @@ import { generateLyrics } from './lyrics'
 
 describe('Generate lyrics', () => {
   test(
+    'Lyrics generation should fail if no api key defined into .env',
+    async () => {
+      const teamSongPayload = {
+        category: 'TestIsAwesome',
+        name: 'DreamTeam',
+        description: 'Our best childhood friends',
+        commonPoints: 'We known each other for a ery long time',
+        musicalGenre: 'rock-n-roll',
+        musicalSpecificity: 'female voice',
+        email: 'mock+test@terascop.com'
+      }
+      expect(() => generateLyrics(teamSongPayload, undefined)).toThrow('No API_KEY provided')
+    }
+  )
+
+  test(
     'TeamSong: Generate some lyrics which contains some provided information data',
     async () => {
       const teamSongPayload = {
@@ -14,9 +30,10 @@ describe('Generate lyrics', () => {
         musicalSpecificity: 'female voice',
         email: 'mock+test@terascop.com'
       }
-      const result = await generateLyrics(teamSongPayload)
+      const result = await generateLyrics(teamSongPayload, Bun.env['ANTHROPIC_API_KEY'])
       expect(result).toBeTruthy()
-      expect(result).toContainValues(Object.values(teamSongPayload))
+      expect(result).toContain(teamSongPayload.name)
+      
     }
   )
 })
--- back/src/modules/lyrics/lyrics.ts
+++ back/src/modules/lyrics/lyrics.ts
@@ -3,13 +3,15 @@ import type { TeamSong } from '../../@types/lyrics'
 
 const app = new Hono()
 
-export async function generateLyrics (payload: TeamSong): Promise<string> {
-  return 'some lyrics'
+export async function generateLyrics (payload: TeamSong, apiKey: string | undefined): Promise<string> {
+  if (!apiKey) throw new Error('No API_KEY provided')
+
+  return Object.values(payload).join(' ')
 }
 
 app.post('/', async (c) => {
   const body = await c.req.json()
-  const lyrics = await generateLyrics(body)
+  const lyrics = await generateLyrics(body, Bun.env['ANTHROPIC_API_KEY'])
   return c.json(lyrics, 201)
 })
 
