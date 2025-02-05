f8116ab0e897fe78d044700e90e84511c26e6462
nmellinger <nicolas.mellinger@terascop.com>
2025-02-05 16:13:39 +0100

    creaete api.d.ts to centralize app route types

---
+++ shared/types/api.d.ts
@@ -0,0 +1,5 @@
+import musicApp from '../../back/src/modules/music/music'
+import type { AppType } from '../../back/src/index'
+
+export type MusicRoutes = typeof musicApp
+export type AppType = AppType
