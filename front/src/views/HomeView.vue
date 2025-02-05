b08b10d233c75cede74b9b83c14ade3de032cc48
nmellinger <nicolas.mellinger@terascop.com>
2025-02-05 15:06:24 +0100

    front repo starts from scratch

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    APP HOME
+  </main>
+</template>
+
+<script setup lang="ts">
+import { hc } from 'hono/client'
+import type { MusicRoutes } from '../../../shared/types/music';
+
+const client = hc<MusicRoutes>('http://localhost:3000/music')
+const categories = await client.category.$get()
+
+</script>
+
