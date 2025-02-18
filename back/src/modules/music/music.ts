2416a6afea1a8401f94f48e48750c078f876d5ff
nmellinger <nicolas.mellinger@terascop.com>
2025-02-18 16:26:40 +0100

    correct type on music module routes + lint

--- back/src/modules/music/music.ts
+++ back/src/modules/music/music.ts
@@ -3,39 +3,58 @@ import * as R from 'remeda'
 import { zValidator } from '@hono/zod-validator'
 import Music from '@/entities/music/music'
 import { categoryIdSchema } from './validation'
-import type { MusicCategory } from '@/database/schema/music'
+import type { MusicCategory, MusicQuestion } from '@/database/schema/music'
+
+type PartialMusicCategory = Pick<
+  MusicCategory,
+  'categoryId' | 'name' | 'description'
+>
+
+type PartialAggregatedMusicCategory = PartialMusicCategory & {
+  questions: Pick<
+    MusicQuestion,
+    'isRequired' | 'placeholder' | 'question' | 'questionId'
+  >[]
+}
 
 const routes = new Hono()
-.get('/category', async (c) => {
-  const $musics = await Music.listAvailableCategories()
+  .get('/category', async (c) => {
+    const $musics = await Music.listAvailableCategories()
 
-  const musics: Pick<MusicCategory, 'categoryId' | 'name' | 'description'>[] = $musics.map((item) => R.pick(item, ['categoryId', 'name', 'description']))
-
-  return c.json(musics, 201)
-})
-.get(
-  '/category/:categoryId',
-  zValidator(
-    'param',
-    categoryIdSchema,
-  ),
-  async (c) => {
-  const { categoryId } = c.req.valid('param')
-
-  const $music = new Music(categoryId)
-  const $musicCategory = await $music.category
-
-  const musicCategory = {
-    ...R.pick($musicCategory, ['categoryId', 'description', 'name']),
-    questions: R.pipe(
-      $musicCategory.questions,
-      R.filter((item) => !item.deprecated),
-      R.map((item) => R.pick(item, ['isRequired', 'placeholder', 'question', 'questionId'])),
+    const musics: PartialMusicCategory[] = $musics.map((item) =>
+      R.pick(item, ['categoryId', 'name', 'description'])
     )
-  }
 
-  return c.json(musicCategory, 201)
-})
+    return c.json(musics, 201)
+  })
+  .get(
+    '/category/specific',
+    zValidator('query', categoryIdSchema),
+    async (c) => {
+      const { categoryId } = c.req.valid('query')
+
+      const $music = new Music(categoryId)
+      const $musicCategory = await $music.category
+
+      const musicCategory: PartialAggregatedMusicCategory = {
+        ...R.pick($musicCategory, ['categoryId', 'description', 'name']),
+        questions: R.pipe(
+          $musicCategory.questions,
+          R.filter((item) => !item.deprecated),
+          R.map((item) =>
+            R.pick(item, [
+              'isRequired',
+              'placeholder',
+              'question',
+              'questionId',
+            ])
+          )
+        ),
+      }
+
+      return c.json(musicCategory, 201)
+    }
+  )
 
 export type MusicRoutes = typeof routes
 
