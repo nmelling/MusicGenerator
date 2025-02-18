ed1d5a80ef8be0b198ddfee84d1fddcfc915dcdb
nmellinger <nicolas.mellinger@terascop.com>
2025-02-18 16:28:08 +0100

    front -- Category -- type assignation + fetch properly category

--- front/src/views/Music/Category.vue
+++ front/src/views/Music/Category.vue
@@ -8,23 +8,17 @@
           <p>{{ category.description }}</p>
         </header>
         <section>
-          <form
-            @submit.prevent="onSubmit"
-          >
-          <fieldset
-            v-for="item of questions"
-            :key="item.questionId"
-            class="fieldset"
-          >
-            <legend class="fieldset-legend">{{ item.question }}</legend>
-            <input
-              v-model="item.answer"
-              class="input"
-              type="text"
-              :placeholder="item.placeholder"
-            />
-            <p v-if="!item.isRequired" class="fieldset-label">Optional</p>
-          </fieldset>
+          <form @submit.prevent="onSubmit">
+            <fieldset v-for="item of questions" :key="item.questionId" class="fieldset">
+              <legend class="fieldset-legend">{{ item.question }}</legend>
+              <input
+                v-model="item.answer"
+                class="input"
+                type="text"
+                :placeholder="item.placeholder"
+              />
+              <p v-if="!item.isRequired" class="fieldset-label">Optional</p>
+            </fieldset>
             <button class="btn btn-primary mt-5" type="submit">Continuez</button>
           </form>
         </section>
@@ -34,65 +28,59 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue'
+import { ref, onMounted, type Ref } from 'vue'
 import { useRoute } from 'vue-router'
-import * as R from 'remeda'
-import { hc } from 'hono/client'
-import type { MusicRoutes } from '../../../../shared/types/api'
-import type { MusicQuestion } from '../../../../shared/types/music'
+import { type InferResponseType } from 'hono/client'
+import { musicClient } from '@/lib/api'
 
 // V1 poc -- need correct validation/patch
 
 const route = useRoute()
 
-const client = hc<MusicRoutes>('http://localhost:3000/api/music')
+type PartialCategoryResponse = InferResponseType<typeof musicClient.category.specific.$get>
 
-type Question = MusicQuestion & { answer: string }
-
-const category = ref(null)
-const questions = ref<Question[]>([])
+const category: Ref<PartialCategoryResponse | null> = ref(null)
+const questions = ref<(PartialCategoryResponse['questions'][number] & { answer: string })[]>([])
 const loading = ref(false)
 
-function setQuestions () {
-  if (!category.value) return []
-
-  return R.pipe(
-    category.value.questions,
-    R.sortBy([R.prop('position'), 'asc']),
-    R.map(({ question }) => question),
-    R.map((question: MusicQuestion) => {
-      const copy = JSON.parse(JSON.stringify(question))
-      return { ...copy, answer: '' }
-    }),
-    R.filter(Boolean),
-  )
-}
-
-onMounted(async () => {
+async function fetchCategory () {
   loading.value = true
+  let categoryId = ''
+  if (typeof route.params.categoryId === 'string') categoryId = route.params.categoryId
+  if (!categoryId) return
+
   try {
-    const res = await client.category[':categoryId'].$get({
-      param: {
-        categoryId: route.params.categoryId
-      }
+    const res = await musicClient.category.specific.$get({
+      query: {
+        categoryId,
+      },
     })
     if (res.ok) {
       category.value = await res.json()
     }
-    questions.value = setQuestions()
   } catch (error) {
+    // todo toast
     console.error(error)
   }
   loading.value = false
+}
+
+function setQuestions () {
+  if (!category.value) return []
+  questions.value = category.value.questions.map((item) => ({
+    ...item,
+    answer: '',
+  }))
+}
+
+onMounted(async () => {
+  await fetchCategory()
+  setQuestions()
 })
-
-
 
 async function onSubmit(): Promise<void> {
   console.log({ questions: questions.value })
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
