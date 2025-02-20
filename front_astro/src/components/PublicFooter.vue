9e6e19551c49b8578ec0932660b49920ba3dc17d
nmellinger <nicolas.mellinger@terascop.com>
2025-02-20 21:40:22 +0100

    Define a basic publici structure -- mix astro/vuejs components --  wip

---
+++ front_astro/src/components/PublicFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <section id="PublicFooter" class="py-15 px-5">
+    Public Footer
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ front_astro/src/components/PublicHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <header
+    id="PublicHeader"
+    class="flex justify-between py-20 px-10"
+  >
+    <img :src="Logo.src"/>
+    <nav>
+      <ul class="flex">
+        <li
+          v-for="nav of navs"
+          :key="navId"
+          class="px-5"
+        >
+          <a :href="nav.to">{{  nav.label  }}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Logo from '../assets/astro.svg'
+
+const navs = ref([
+  { navId: 'home', label: 'Accueil', to: '/' },
+  { navId: 'musics', label: 'Créez votre chanson', to: '/music/categories' },
+  { navId: 'contact', label: 'Contact', to: '/contact' },
+])
+</script>
+
+<style>
+
+</style>
