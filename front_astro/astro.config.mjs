6cfba678ea9747b8ddfc0b4afb54590ec9f757bf
nmellinger <nicolas.mellinger@terascop.com>
2025-02-27 14:45:23 +0100

    astro config ssr + alias src

--- front_astro/astro.config.mjs
+++ front_astro/astro.config.mjs
@@ -2,12 +2,16 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+		mode: 'standalone',
+	}),
   integrations: [vue()],
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
