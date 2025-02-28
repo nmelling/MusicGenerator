8fe20d92e1c5a2403cbfbc9c0c2088a0129933a1
nmellinger <nicolas.mellinger@terascop.com>
2025-02-28 09:43:51 +0100

    init & set form correctly

--- front_astro/src/components/music/CategoryForm.vue
+++ front_astro/src/components/music/CategoryForm.vue
@@ -1,22 +1,39 @@
 <template>
-  <form>
+  <form
+    class="flex flex-col"
+  >
+    <label class="flex flex-col py-5">
+      <span>Adresse email</span>
+      <input
+        type="email"
+        required
+        placeholder="adresse@mail.com"
+        :value="form.email"
+        @input="(e) => setEmail((e.target as HTMLInputElement).value)"
+      />
+    </label>
     <label
-      v-for="question of questions"
+      v-for="(question, index) in questions"
       :key="question.questionId"
+      class="flex flex-col py-5"
     >
       <span>{{ question.question }}</span>
       <input
         type="text"
         :placeholder="question.placeholder"
-
+        :required="question.isRequired"
+        @input="(e) => setAnswer((e.target as HTMLInputElement).value, index)"
+        :value="form.answers[index]?.answer"
       />
     </label>
   </form>
 </template>
 
 <script setup lang="ts">
-import { ref, type Ref, onMounted } from 'vue'
+import { onMounted } from 'vue'
+import { useStore } from '@nanostores/vue'
 import type { CategoryResponse } from '@/stores/music'
+import { $categoryForm, initCategoryForm, setAnswer, setEmail } from '@/stores/music'
 
 const props = defineProps<{
   questions: CategoryResponse["questions"][number][],
@@ -24,8 +41,10 @@ const props = defineProps<{
 }>()
 
 onMounted(() => {
-
+  initCategoryForm({ categoryId: props.categoryId, questions: props.questions })
 })
+
+const form = useStore($categoryForm)
 
 </script>
 
--- front_astro/src/stores/music.ts
+++ front_astro/src/stores/music.ts
@@ -1,15 +1,36 @@
 import { hc, type InferResponseType } from 'hono/client'
-import { atom } from 'nanostores';
-import * as R from 'remeda'
+import { deepMap } from 'nanostores'
 
 import type { MusicRoutes } from '@/../../back/src/modules/music/music'
 import type { NewOrderPayload } from '@/../../back/src/modules/order/validation'
 
 export const client = hc<MusicRoutes>('http://localhost:3000/api/music')
-export type CategoryResponse = InferResponseType<typeof client.category.specific.$get>
+export type CategoryResponse = InferResponseType<
+  typeof client.category.specific.$get
+>;
 
-const $categoryForm = atom({})
+export const $categoryForm = deepMap<
+  Partial<NewOrderPayload> & Pick<NewOrderPayload, 'answers'>
+>({
+  categoryId: undefined,
+  email: '',
+  answers: [],
+});
 
-export function initCategoryForm ({ categoryId, questions }: Pick<CategoryResponse, 'categoryId' | 'questions'>): void {
-  $categoryForm.set()
+export function initCategoryForm({
+  categoryId,
+  questions,
+}: Pick<CategoryResponse, 'categoryId' | 'questions'>): void {
+  $categoryForm.setKey('categoryId', categoryId)
+  $categoryForm.setKey(
+    'answers',
+    questions.map((question) => ({
+      questionId: question.questionId,
+      answer: '',
+    }))
+  );
+}
+
+export function setAnswer(answer: string, index: number): void {
+  $categoryForm.setKey(`answers[${index}].answer`, answer)
 }
